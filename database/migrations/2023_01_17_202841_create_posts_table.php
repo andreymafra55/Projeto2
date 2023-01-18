c54ac5890f371838e334ff9158f15d3c4614ef61
andreymafra55 <andreymafra55@gmail.com>
2023-01-18 01:29:29 -0300

    configuracao das tabelas do post

---
+++ database/migrations/2023_01_17_202841_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->binary('photo');
+            $table->string('region');
+            $table->string('phone');
+            $table->string('needed');
+            $table->string('address');
+            $table->text('description');
+            $table->foreignId('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
